51c6c0ca747d0e00dfc84b78307235c460a3c734
lewardo <segfault.ninja@gmail.com>
2023-08-06 15:22:05 +0100

    type definitions for library

---
+++ include/types.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace ml {
+namespace types {
+
+using float_type = float;
+
+template <typename T = float_type> using vector = tensor<T, 1>;
+template <typename T = float_type> using maxtrix = tensor<T, 2>;
+
+} // namespace types
+} // namespace ml
