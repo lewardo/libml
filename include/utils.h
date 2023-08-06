5abaa7d0178e5f84985bd54e575941bacaf123ac
lewardo <segfault.ninja@gmail.com>
2023-08-06 15:22:23 +0100

    global utilities

---
+++ include/utils.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "types.h"
+
+#define ifnt(c) if (!(c))
+
+namespace ml {
+namespace utils {
+namespace detail {
+
+using namespace types;
+
+template <typename T> struct default_value {
+  static constexpr typename std::decay_t<T> value = std::decay_t<T>{};
+};
+
+template <typename T> using default_value_v = typename default_value<T>::value;
+
+} // namespace detail
+
+using detail::default_value, detail::default_value_v;
+
+} // namespace utils
+
+} // namespace ml
