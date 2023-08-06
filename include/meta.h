ac6c481dac5fa86f02e1eded69d623a0568bc0a1
lewardo <segfault.ninja@gmail.com>
2023-08-06 23:42:49 +0100

    added metaprogramming utilities

--- include/meta.h
+++ include/meta.h
@@ -55,10 +55,14 @@ constexpr auto make_satisfy_sequence_impl(sequence<Is...>) {
 }
 
 template <size_t N, typename... Args>
-constexpr auto total_equals = (sizeof...(Args) == N);
+struct total_equals {
+  static constexpr const auto value = (sizeof...(Args) == N);
+};
 
 template <typename T, typename... Args>
-constexpr auto count_same = ((... + (std::same_as<T, Args> ? 1 : 0)));
+struct count_same {
+  static constexpr const auto value = ((... + (std::same_as<T, Args> ? 1 : 0)));
+};
 
 template <template <typename, typename> class P, typename T, typename... Ts>
 using all_satisfy = std::conjunction<P<T, Ts>...>;
@@ -69,7 +73,10 @@ using all_either =
     std::conjunction<std::disjunction<Pa<Ta, Ts>, Pb<Tb, Ts>>...>;
 
 template <typename T, typename... Ts>
-using all_same = all_satisfy<std::is_same, T, Ts...>;
+using all_same = std::conjunction<std::is_same<T, Ts>...>;
+
+template <typename T, typename... Ts>
+using all_convertible = std::conjunction<std::is_convertible<T, Ts>...>;
 
 template <size_t N>
 using index_sequence_t = decltype(detail::make_index_sequence_impl<N>());
@@ -98,7 +105,8 @@ template <template <typename, typename> class P, typename T, typename... Ts>
 constexpr auto satisfy_sequence = satisfy_sequence_t<P, T, Ts...>{};
 };  // namespace detail
 
-using detail::all_satisfy, detail::all_either, detail::all_same;
+using detail::all_satisfy, detail::all_either, detail::all_same,
+    detail::all_convertible;
 using detail::index_sequence_t, detail::index_sequence;
 using detail::range_sequence_t, detail::range_sequence;
 using detail::repeat_sequence_t, detail::repeat_sequence;
