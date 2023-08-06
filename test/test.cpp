dbab7f40e733fbc8433304b5b1ecc36252d6c962
lewardo <segfault.ninja@gmail.com>
2023-08-06 23:41:23 +0100

    tensor test

--- test/test.cpp
+++ test/test.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <vector>
 
 #include "libml.h"
+
+libml_using(types);
 
 auto main() -> int {
   test();
   std::cout << TEST << std::endl;
+
+  detail::tensor_traits<3> traits({2, 3, 4});
+
+  std::cout << traits.size << std::endl;
+  std::cout << traits.calculate_offset(1, 2, 3) << std::endl;
 
   return EXIT_SUCCESS;
 }
