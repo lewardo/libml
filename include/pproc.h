7e4c4227c41d33eb70d41adfbef5674177e67c10
lewardo <segfault.ninja@gmail.com>
2023-08-06 15:21:48 +0100

    preprocessor utilities from libml_legacy

---
+++ include/pproc.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define STRINGIZE(s) __STRINGIZE1(s)
+#define __STRINGIZE1(s) __STRINGIZE2(s)
+#define __STRINGIZE2(s) #s
+
+#define CONCATENATE(a, b) __CONCATENATE1(a, b)
+#define __CONCATENATE1(a, b) __CONCATENATE2(a, b)
+#define __CONCATENATE2(a, b) a##b
+
+#define ML_USING_DECL(NS) using namespace ::ml::NS;
+#define libml_using(...)                                                       \
+  using namespace ::ml;                                                        \
+  FOR_EACH(ML_USING_DECL, __VA_ARGS__)
